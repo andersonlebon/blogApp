0168421d5377c21187bab8ec5591b539b47cda8e
Caleb Anderson <buyananderson@gmail.com>
2021-11-03 16:05:47 +0200

    Create the show post view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   get 'users/:user_id/posts/:id', to: "posts#show"
   get 'users/', to: "users#index"
   get 'users/:id', to: "users#index"
-  
-  get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
