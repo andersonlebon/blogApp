314b27b41f4d3f672652273d82e94c90b378e329
Caleb Anderson <buyananderson@gmail.com>
2021-11-03 16:31:48 +0200

    Set the default route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'users/:user_id/posts/:id', to: 'posts#show'
   get 'users/', to: 'users#index'
   get 'users/:id', to: 'users#show'
+  root 'users/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
