0fe3eb066881e1de06942b0c382eede8cdbcf0f7
Caleb Anderson <buyananderson@gmail.com>
2021-11-03 16:11:04 +0200

    Correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'users/:user_id/posts', to: "posts#index"
   get 'users/:user_id/posts/:id', to: "posts#show"
   get 'users/', to: "users#index"
-  get 'users/:id', to: "users#index"
+  get 'users/:id', to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
