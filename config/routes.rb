6d06f86b9920aee69bda340436c9927bdd4ffd96
Caleb Anderson <buyananderson@gmail.com>
2021-11-03 16:37:41 +0200

    Set the default route 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'users#index'
   get 'users/:user_id/posts', to: 'posts#index'
   get 'users/:user_id/posts/:id', to: 'posts#show'
   get 'users/', to: 'users#index'
   get 'users/:id', to: 'users#show'
-  root 'users/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
