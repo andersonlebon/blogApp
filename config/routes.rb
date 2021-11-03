4d425fe8c334293345a1d815690598e93c10fe43
Caleb Anderson <buyananderson@gmail.com>
2021-11-03 15:52:19 +0200

    Create User router

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/:user_id/posts', to: "posts#index"
+  get 'users/:user_id/posts', to: "posts#index"
+  get 'users/:user_id/posts', to: "posts#index"
+  get 'users/:user_id/posts', to: "posts#index"
+  
   get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
