d526a98bfc851489ef5638f0bef354fefb7f0919
Caleb Anderson <buyananderson@gmail.com>
2021-11-03 15:57:17 +0200

    Add a route to see the user post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/:user_id/posts', to: "posts#index"
-  get 'users/:user_id/posts', to: "posts#index"
-  get 'users/:user_id/posts', to: "posts#index"
+  get 'users/:user_id/posts/:id', to: "posts#show"
+  get 'users/", to: "users#index"
   get 'users/:user_id/posts', to: "posts#index"
   
   get 'posts/index'
