3011af288ce81607d0ba1f0dcc971034ecb7339f
Caleb Anderson <buyananderson@gmail.com>
2021-11-04 11:27:34 +0200

    Correct tests for users posts

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
-    before(:example) { get user_posts_path(10) }
+    before(:example) { get users_path(10) }
 
     it 'should return correct response status' do
       expect(response).to have_http_status(200)
@@ -13,23 +13,23 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it 'should include correct text' do
-      expect(response.body).to include('Here is a list of posts for a given user')
+      expect(response.body).to include('Show All users')
     end
   end
 
-  describe 'GET #show' do
-    before(:example) { get user_post_path(10, 1) }
+#   describe 'GET #show' do
+#     before(:example) { get users_post_path(10, 1) }
 
-    it 'should return correct response status' do
-      expect(response).to have_http_status(200)
-    end
+#     it 'should return correct response status' do
+#       expect(response).to have_http_status(200)
+#     end
 
-    it 'should render correct template' do
-      expect(response).to render_template(:show)
-    end
+#     it 'should render correct template' do
+#       expect(response).to render_template(:show)
+#     end
 
-    it 'should include correct  text' do
-      expect(response.body).to include('Show the details of one post per user')
-    end
-  end
+#     it 'should include correct  text' do
+#       expect(response.body).to include('Show the details of one post per user')
+#     end
+#   end
 end
