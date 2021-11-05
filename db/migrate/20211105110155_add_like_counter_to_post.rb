a4c2f19c9d3e2ba58241d8aa61f3eb204c9706c7
Caleb Anderson <buyananderson@gmail.com>
2021-11-05 13:56:39 +0200

    Genarate the schema

--- db/migrate/20211105110155_add_like_counter_to_post.rb
+++ db/migrate/20211105110155_add_like_counter_to_post.rb
@@ -1,4 +1,4 @@
-class AddLikeCounterToPost < ActiveRecord::Migration[6.1]
+class AddLikeCounterToPostTable < ActiveRecord::Migration[6.1]
   def change
     add_column :posts, :like_counter, :integer
   end
