0f20e3cd167b32515d2c8022d265ff04b6ade431
Caleb Anderson <buyananderson@gmail.com>
2021-11-12 22:51:34 +0200

    Make the create user controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,18 @@ class PostsController < ApplicationController
     @post = @user.posts
   end
 
+  # create a new post
+  def create
+    @user = User.find(params[:user_id]) # find the user
+    @post = @user.posts.create(post_params) # create a new post for the user
+    redirect_to user_posts_path(@user) # redirect to the user's posts
+  end
+  
+
+  def new
+    @post = Post.new
+  end
+
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
