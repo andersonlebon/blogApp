a9bbdaa84bb78724cf3e59ed6754cf0667cd99e8
Caleb Anderson <buyananderson@gmail.com>
2021-11-13 09:11:19 +0200

    Create Like model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,21 +3,35 @@ class CommentsController < ApplicationController
   # create comment for a post
 # new comment
 
-  def new
-    @user = User.find(params[:user_id])
-    @comment = @user.posts.find(params[:post_id]).comments.new
-  end
+    def new
+      @user = User.find(params[:user_id])
+      @comment = @user.posts.find(params[:post_id]).comments.new
+    end
 
-  def create
-    @user = User.find(params[:user_id])
-    @comment = @user.posts.find(params[:post_id]).comments.create(comment_params)
-    redirect_to post_path(@comment.post)
-  end
+    # def create
+    #   @user = User.find(params[:user_id])
+    #   @comment = @user.posts.find(params[:post_id]).comments.create(comment_params)
+    #   # redirect_to user_post_path(@user, @post)
+    #   redirect_to user_post_path(@user, @comment.post)
+    # end
 
-  private
-  def comment_params
-    params.require(:comment).permit(:text)
-  end
+
+    def create
+        @comment = Comment.new(comment_params)
+        @user = User.find(params[:user_id])
+        @comment.user_id = params[:user_id]
+        @comment.post_id = params[:post_id]
+        if @comment.save
+          redirect_to user_post_path(@comment.user, @comment.post)
+        else
+          render 'new'
+        end
+    end
+
+    # private
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
  
 
 
