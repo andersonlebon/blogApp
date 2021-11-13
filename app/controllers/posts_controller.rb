aae2e1f6bce056f4f9f4b5883f47bfc81f37bf14
Caleb Anderson <buyananderson@gmail.com>
2021-11-13 12:23:09 +0200

    Correct linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts 
+    @posts = @user.posts
 
     @post_comments = @posts.map do |post|
-     {:id => post.id, :comments => post.comments.all}
+      { id: post.id, comments: post.comments.all }
     end
-
   end
-
 
   # create a new post
   def create
@@ -16,20 +14,18 @@ class PostsController < ApplicationController
     @post = @user.posts.create(post_params) # create a new post for the user
     redirect_to user_posts_path(@user) # redirect to the user's posts
   end
-  
 
   # new post
 
   def new
     @user = User.find(params[:user_id])
-    @post = @user.posts.new 
+    @post = @user.posts.new
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
-    @comments = @post.comments  # find all comments for this post
-
+    @comments = @post.comments # find all comments for this post
   end
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users do
-    resources :posts do 
+    resources :posts do
       resources :comments
       resources :likes
     end
