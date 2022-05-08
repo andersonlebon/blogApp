dd245a1121fedc55580aba50d9d0c68311f06aa2
anderson <buyananderson@gmail.com>
2022-05-08 23:47:14 +0200

    Remove commentes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
   before_action :authenticate_user!
-  # get he current user
+  # get the current user
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
